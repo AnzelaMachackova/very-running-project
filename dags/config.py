e4e6eab47061ef23677b6681d371a9f0b622ad8f
AnzelaMachackova <angmachy@gmail.com>
2024-04-10 14:57:09 +0200

    initial commit

---
+++ dags/config.py
@@ -0,0 +1,4 @@
+KAGGLE2BQ_DAG = 'kaggle_to_gcs'
+BUCKET_NAME = ''
+FILE_NAME = ''
+K_DATASET = ''
---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "/Users/anzelam/ac/google/terraform/gcp-creds.json"
+}
+
+variable "project" {
+  description = "Project"
+  default     = "de-running-project"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "europe-west3-a"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "EU"
+}
+
+variable "bq_dataset_name_stage" {
+  description = "My BigQuery Dataset Name"
+  default     = "stage"
+}
+
+variable "bq_dataset_name_report" {
+  description = "My BigQuery Dataset Name"
+  default     = "report"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  default     = "de-running-project-bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
